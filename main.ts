6db16047876c659bbc6ed40f35b9f42785e1a4f0
Robin Steeman <robin@tracefy.com>
2022-10-01 14:52:21 +0200

    added indicators

--- main.ts
+++ main.ts
@@ -1,23 +1,12 @@
 import { config as env } from "https://deno.land/std@0.158.0/dotenv/mod.ts";
 import { FtxClient } from "./src/exchanges/ftx/client.ts"
+import { williamsFractals } from "./src/indicators/fractals.ts"
+import { ichimoku } from "./src/indicators/ichimoku.ts"
 
 const VARIABLES = await env()
 
-// const config = {
-//     headers: {
-//         'accept': 'application/json',
-//         'Content-Type': 'application/json; utf-8',
-//         'FTX-KEY': VARIABLES.FTX_API_KEY,
-//         'FTX-SUBACCOUNT': VARIABLES.FTX_SUBACCOUNT
-//     }
-// }
-
-// const now = Date.now()
-// config.headers['FTX-TS'] = now
-// const dataQueryString = now + 'GET' + '/api/subaccounts'
-// console.log(dataQueryString)
-// config.headers['FTX-SIGN'] = hmac('sha256', VARIABLES.FTX_API_SECRET, dataQueryString, 'utf8', 'hex')
-
 const ftxClient = new FtxClient(VARIABLES.FTX_API_KEY, VARIABLES.FTX_API_SECRET, VARIABLES.FTX_SUBACCOUNT)
 
-console.log(await ftxClient.apiRequest('GET', '/subaccounts'))
+console.log(await ichimoku('4h'))
+
+// console.log(await ftxClient.apiRequest('GET', '/markets/BTC-PERP'))
